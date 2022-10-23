ca2a6b88862ff3eda619c5f5b3b4fea785ce5f81
FernandoGarcia1995 <fergarcia1995cuenta@gmail.com>
2022-10-23 15:19:15 +0200

    ﻿cambio 2 - test_2

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -47,6 +47,7 @@ public class Main {
 				check = false;
 			
 			System.out.printf("pR : %d - %s \n", contPT,currentItem[0]);
+			System.out.printf("pN : %d - %s \n", contPT,currentItem[0]);
 			System.out.printf("es : %d - %s \n", contES,currentItem[0]);
 			System.out.println();
 		}
